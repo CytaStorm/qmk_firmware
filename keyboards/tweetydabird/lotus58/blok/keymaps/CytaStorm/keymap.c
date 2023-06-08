8952c8f73adb8c0bb488e94aedcf4455e3e26ce1
Jeff <cytastorm@gmail.com>
2023-06-08 17:02:06 -0400

    updated keymap for 6-0 on numrow on right side of keyboard

--- keyboards/tweetydabird/lotus58/blok/keymaps/CytaStorm/keymap.c
+++ keyboards/tweetydabird/lotus58/blok/keymaps/CytaStorm/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [1] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,   KC_4,   KC_5,            _______,_______,          KC_6,    KC_7,    KC_8,    KC_9, KC_0, KC_GRAVE,
+  KC_ESC,   KC_6,   KC_7,    KC_8,   KC_9,   KC_0,            _______,_______,          KC_6,    KC_7,    KC_8,    KC_9, KC_0, KC_GRAVE,
   KC_F1, KC_F2,   KC_F3,  KC_F4, KC_F5, KC_F6,                         KC_F7,    KC_F8,    KC_F9,    KC_F10, KC_F11, KC_F12,
   _______, KC_EXCLAIM, KC_AT, KC_HASH, KC_DOLLAR, KC_PERCENT,                        KC_CIRCUMFLEX,    KC_AMPERSAND,    KC_ASTERISK,    KC_LEFT_PAREN, KC_RIGHT_PAREN,  KC_PIPE,
   _______, _______, _______,  _______, _______,       _______,  _______, _______, _______,    _______, _______, KC_LBRC, KC_RBRC,_______,
